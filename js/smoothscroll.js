9cdabac7e9664f466c7b2fb685393445942dff78
thusithak2 <thusitha32@gmail.com>
2026-02-20 09:50:25 +0530

    cass changes

--- js/smoothscroll.js
+++ js/smoothscroll.js
@@ -4,7 +4,7 @@ gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 let smoother = ScrollSmoother.create({
   wrapper: "#smooth-wrapper",
   content: "#smooth-content",
-  smooth: 1.5, // Time (seconds) it takes to "catch up" to the scroll
+  smooth: 0.5, // Time (seconds) it takes to "catch up" to the scroll
   effects: true, // Look for data-speed and data-lag attributes
   smoothTouch: 0.1, // Optional: enable smooth scroll on touch devices
 });
