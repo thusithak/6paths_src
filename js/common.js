7c1a17ddd5800d701cc7500b9528eeabe08eca5f
thusithak <thusitha32@yahoo.com>
2026-04-05 10:56:10 +0530

    copy animation update

--- js/common.js
+++ js/common.js
@@ -156,7 +156,7 @@ function runAdhocIntegrations() {
       if (copyBtn) {
         copyBtn.addEventListener('click', async () => {
             const emailToCopy = copyBtn.getAttribute('data-email');
-    
+            copyBtn.event.preventDefault(); 
             try {
                 await navigator.clipboard.writeText(emailToCopy);
                 triggerConfetti(copyBtn);
